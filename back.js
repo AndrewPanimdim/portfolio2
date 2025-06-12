c8fc0a72b32f6c0ec9c5b56e00b0188a84334f3b
Andrew Panimdim <baelmeans74@gmail.com>
2025-06-12 14:52:25 +0800

    popup

---
+++ back.js
@@ -0,0 +1,19 @@
+function Animation(selector, animationClass = 'animate', threshold = 0.5) {
+      const element = document.querySelectorAll(selector);
+      const observer = new IntersectionObserver((entries, observerInstance) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(animationClass);
+            observerInstance.unobserve(entry.target);
+          }
+        });
+      }, { threshold });
+      element.forEach(el => observer.observe(el));
+    }
+
+Animation('#AndrewsInformation')
+
+
+
+
+
