f7d8ead37c7b6c1ddb13d86a498a05b743c3eb9c
Andrew Panimdim <baelmeans74@gmail.com>
2025-06-25 03:36:38 +0800

    aa

---
+++ Main/calculator/js.js
@@ -0,0 +1,32 @@
+function appendValue(clickedDiv) {
+    const value = clickedDiv.textContent;
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent += value;
+}
+
+
+function clearResult() {
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = "";
+}
+
+
+function deleteLastInput() {
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = resultDiv.textContent.slice(0, -1);
+}
+
+
+function calculateResult() {
+    const resultDiv = document.getElementById("result");
+    try {
+        const result = eval(resultDiv.textContent);
+        resultDiv.textContent = result;
+    } catch (error) {
+        resultDiv.textContent = "Syntax error";
+    }
+}
+
+
+
+
